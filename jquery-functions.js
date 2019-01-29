85bffcda23af662fe0578ec3d41ebba54e0184d8
Pedro Becker <phgbecker@gmail.com>
2019-01-29 16:19:26 -0200

    1st commit

---
+++ jquery-functions.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    // Fade in the signIn div
+    $('#signInBox').fadeIn(500);
+
+    $('#signInAction').submit(function (event) {
+        // Consume the default event
+        event.preventDefault();
+
+        /**
+         * Validate a series of user inputs
+         */
+        if (!$('#usernameInput').val()) {
+            $('#usernameValidation').text('Login field is mandatory').show().fadeOut(1500);
+            return;
+        }
+
+        if ($('#usernameInput').val().length <= 3) {
+            $('#usernameValidation').text('Login field length should be bigger than 3').show().fadeOut(1500);
+            return;
+        }
+
+        if (!$('#passwordInput').val()) {
+            $('#passwordValidation').text('Password field is mandatory').show().fadeOut(1500);
+            return;
+        }
+
+        if ($('#passwordInput').val().length <= 5) {
+            $('#passwordValidation').text('Password field length should be bigger than 5').show().fadeOut(1500);
+            return;
+        }
+
+        // If got this far, give the use a sign in message
+        $('#signInValidation').text('Login successful!').show().fadeOut(3000);
+    });
+
+});
